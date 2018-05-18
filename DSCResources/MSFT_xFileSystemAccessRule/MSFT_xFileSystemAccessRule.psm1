6fb4ba2befe7250b2d222d597f79fb77e31b0b13
Dan Reist <dareist@microsoft.com>
2018-05-18 07:53:29 -0400

    Fixed typo

--- DSCResources/MSFT_xFileSystemAccessRule/MSFT_xFileSystemAccessRule.psm1
+++ DSCResources/MSFT_xFileSystemAccessRule/MSFT_xFileSystemAccessRule.psm1
@@ -105,7 +105,7 @@ function Get-TargetResource
         Present to create the rule, Absent to remove an existing rule. Default value is 'Present'.
 
     .PARAMETER ProcessOnlyOnActiveNode
-        Specifies that the resource will only determine if a change is needed if the target node is the active host of the filesystem object. The user the configuration is run as must haver permission to the Windows Server Failover Cluster.
+        Specifies that the resource will only determine if a change is needed if the target node is the active host of the filesystem object. The user the configuration is run as must have permission to the Windows Server Failover Cluster.
         Not used in Set-TargetResource.
 #>
 function Set-TargetResource
@@ -227,7 +227,7 @@ function Set-TargetResource
         Present to create the rule, Absent to remove an existing rule. Default value is 'Present'.
 
     .PARAMETER ProcessOnlyOnActiveNode
-        Specifies that the resource will only determine if a change is needed if the target node is the active host of the filesystem object. The user the configuration is run as must haver permission to the Windows Server Failover Cluster.
+        Specifies that the resource will only determine if a change is needed if the target node is the active host of the filesystem object. The user the configuration is run as must have permission to the Windows Server Failover Cluster.
 #>
 function Test-TargetResource
 {
